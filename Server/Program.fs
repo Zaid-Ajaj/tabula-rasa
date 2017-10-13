57ea2879d7ec0bb8e62b64e44159a2832eccd7be
Zaid-Ajaj <zaid.naom@gmail.com>
2017-10-13 02:19:44 +0200

    Added default cuteness :smile:

--- Server/Program.fs
+++ Server/Program.fs
@@ -1,6 +1,26 @@
 ﻿open System
 
+open Suave
+open Suave.Files
+open Suave.Operators
+open System.IO
+open System.Reflection
+
+let (</>) x y = Path.Combine(x, y) 
+
+let rec findRoot dir =
+    if File.Exists(IO.Path.Combine(dir, "TabulaRasa.sln"))
+    then dir
+    else
+        let parent = Directory.GetParent(dir)
+        if isNull parent then
+            failwith "Couldn't find package.json directory"
+        findRoot parent.FullName
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let cwd = Assembly.GetEntryAssembly().Location
+    let root = findRoot cwd
+    let client = root </> "Client" </> "src"
+    printfn "Hello %s from F#!" client
     0 // return an integer exit code

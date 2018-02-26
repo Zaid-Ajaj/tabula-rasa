c12f58e2ed0c34b3191160fbe20fe1ccb96b999c
Zaid <zaid.naom@gmail.com>
2018-02-26 09:46:15 +0100

    Validation for adding posts and tests

--- Server.Tests/StorageTests.fs
+++ Server.Tests/StorageTests.fs
@@ -3,24 +3,53 @@ module StorageTests
 open Expecto
 open LiteDB
 open LiteDB.FSharp
+open Shared.ViewModels
 open System.IO
 
 let pass() = Expect.isTrue true "passed"
 let fail() = Expect.isTrue false "failed"
+let failedWith msg = Expect.isTrue false msg
 
-let mapper = FSharpBsonMapper()
-let memory = new MemoryStream()
-let database = new LiteDatabase(memory, mapper)
 
+let withDatabase (f: LiteDatabase -> unit) = 
+  let mapper = FSharpBsonMapper()
+  let memory = new MemoryStream()
+  let database = new LiteDatabase(memory, mapper)
+  f database  
+  
 let storageTests = 
     testList "Storage tests" [
-        testCase "saveFile and readFile work" <| fun _ -> 
-           let content = "hello there"
-           Storage.saveFile "hello.txt" content database |> ignore
-           match Storage.readFile "hello.txt" database  with
-           | Some "hello there" -> pass()
-           | otherwise -> fail()
-           match Storage.readFile "non-existent.txt" database with 
-           | None -> pass()
-           | otherwise -> fail()
-    ]
+        testCase "saveFile and readFile work from the database" <| fun _ -> 
+          withDatabase <| fun database ->
+            let content = "hello there"
+            Storage.saveFile "hello.txt" content database |> ignore
+            match Storage.readFile "hello.txt" database  with
+            | Some "hello there" -> pass()
+            | otherwise -> fail()
+            match Storage.readFile "non-existent.txt" database with 
+            | None -> pass()
+            | otherwise -> fail()
+            
+        testCase "Error message is returned with trying to publish existing post" <| fun _ ->
+          withDatabase <| fun database -> 
+            let blogPostReq : NewBlogPostReq = 
+              { Title = "title"; Slug = "slug"; Tags = []; Content = "irrelevant" }
+            
+            match BlogPosts.publishPost blogPostReq database with 
+            | Error _ -> failedWith "Shouldn't get an error message just yet"
+            | Ok id -> 
+              // post added, now add again     
+              let sameBlogDifferentSlug = { blogPostReq with Slug = "something" } 
+              match BlogPosts.publishPost sameBlogDifferentSlug database with
+              | Ok _ -> failedWith "Shouldn't add a new post with the same title" 
+              | Error "A post with title 'title' already exists" -> 
+                  // then this is the correct error
+                  // now check the slug 
+                  let sameSlugWithDifferentTitle = { blogPostReq with Title = "something" }
+                  match BlogPosts.publishPost sameSlugWithDifferentTitle database with
+                  | Ok _ -> failedWith "Shouldn't add a new post with the same slug"
+                  | Error "A post with slug 'slug' already exists" -> pass() 
+                  | Error otherError -> failwith "Shouldn't fail with now" 
+              | Error otherError -> failwith "Shouldn't fail with now" 
+        ]
+       
--- Server/BlogPosts.fs
+++ Server/BlogPosts.fs
@@ -22,34 +22,64 @@ type BlogPost = {
     Reactions: (SocialReaction * int) list
 }
 
-let create (req: NewBlogPostReq) (db : LiteDatabase) = 
-    let newPost : BlogPost = {
-         Id = 0 // by default, it will be auto incremented
-         Title = req.Title
-         Content = req.Content
-         Slug = req.Slug
-         Tags = req.Tags
-         DateAdded = DateTime.Now
-         Reactions = []
-         Comments = []
-         IsDraft = false
-         IsFeatured = false
-      } 
-    
+let toBlogPost (req : NewBlogPostReq) = 
+    {  Id = 0 // by default, it will be auto incremented
+       Title = req.Title
+       Content = req.Content
+       Slug = req.Slug
+       Tags = req.Tags
+       DateAdded = DateTime.Now
+       Reactions = []
+       Comments = []
+       IsDraft = false
+       IsFeatured = false }
+
+let postAlreadyPublished (req: NewBlogPostReq) (db : LiteDatabase) = 
     let posts = db.GetCollection<BlogPost> "posts"
-    try 
-      let result : BsonValue = posts.Insert(newPost)
-      Some (Bson.deserializeField<int> result)
-    with 
-    | _ -> None
-        
-let publishNewPost (req: SecureRequest<NewBlogPostReq>) (database: LiteDatabase)   = 
+    let noDraft = Query.EQ("IsDraft", BsonValue(false))
+    let byTitle = Query.EQ("Title", BsonValue(req.Title)) 
+    posts.Find(Query.And(noDraft, byTitle)) 
+    |> Seq.tryHead
+    |> function 
+        | Some _ -> Some (sprintf "A post with title '%s' already exists" req.Title) 
+        | None -> 
+            let bySlug = Query.EQ("Slug", BsonValue(req.Slug)) 
+            posts.Find(Query.And(noDraft, bySlug)) 
+            |> Seq.tryHead 
+            |> function 
+                | Some _ -> Some (sprintf "A post with slug '%s' already exists" req.Slug)
+                | None -> None 
+                 
+let publishPost (req: NewBlogPostReq) (db : LiteDatabase) = 
+  let posts = db.GetCollection<BlogPost> "posts"
+  try 
+    match postAlreadyPublished req db with
+    | Some errorMessage -> Error errorMessage 
+    | None -> 
+        let newPost = toBlogPost req
+        let result : BsonValue = posts.Insert(newPost)
+        Ok (Bson.deserializeField<int> result)
+  with 
+  | ex -> Error "Could not add the new blog post to the database"
+      
+let publishNewPost (req: SecureRequest<NewBlogPostReq>) (database: LiteDatabase) = 
+   match Security.validateJwt req.Token with
+   | None -> Error "Authorization token was invalid"
+   | Some user -> publishPost req.Body database   
+          
+let saveAsDraft (req: SecureRequest<NewBlogPostReq>) (database: LiteDatabase) = 
    match Security.validateJwt req.Token with
    | None -> Error "Authorization token was invalid"
    | Some user ->
-      match create req.Body database with
-      | None -> Error "Could not add the new blog post to the database"
-      | Some id -> Ok id                 
+       let posts = database.GetCollection<BlogPost> "posts"
+       let draft = { toBlogPost req.Body with IsDraft = true }
+       try 
+        let result : BsonValue = posts.Insert(draft)
+        Ok (Bson.deserializeField<int> result) 
+       with 
+       | ex -> 
+         // TODO: log ex 
+         Error "Could not add the draft to the database"                   
     
 let toBlogPostItem (post: BlogPost) = 
     { Id = post.Id; 

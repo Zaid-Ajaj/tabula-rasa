13a3968beeda574c63ca8be96c8597c8c3c2a251
sharno <sharnoby3@gmail.com>
2021-09-19 14:51:45 -0400

    formatting and make Admin form submittable with Enter key

--- Client/src/App/Admin/Login/View.fs
+++ Client/src/App/Admin/Login/View.fs
@@ -11,16 +11,19 @@ type InputType =
     | Text
     | Password
 
-let textInput inputLabel initial inputType (onChange : string -> unit) =
+let textInput inputLabel initial inputType (onChange: string -> unit) =
     let inputType =
         match inputType with
         | Text -> "input"
         | Password -> "password"
-    div [ ClassName "form-group" ] [ input [ ClassName "form-control form-control-lg"
-                                             Type inputType
-                                             DefaultValue initial
-                                             Placeholder inputLabel
-                                             OnChange(fun e -> onChange !!e.target?value) ] ]
+
+    div [ ClassName "form-group" ] [
+        input [ ClassName "form-control form-control-lg"
+                Type inputType
+                DefaultValue initial
+                Placeholder inputLabel
+                OnChange(fun e -> onChange !!e.target?value) ]
+    ]
 
 let loginFormStyle =
     Style [ Width "400px"
@@ -33,45 +36,59 @@ let cardBlockStyle =
             BorderRadius 10 ]
 
 [<Emit("null")>]
-let emptyElement : ReactElement = jsNative
+let emptyElement: ReactElement = jsNative
 
 let errorMessagesIfAny triedLogin =
-    function 
+    function
     | [] -> emptyElement
     | _ when not triedLogin -> emptyElement
-    | errors -> 
-        let errorStyle =
-            Style [ Color "crimson"
-                    FontSize 12 ]
-        ul [] [ for error in errors -> li [ errorStyle ] [ str error ] ]
+    | errors ->
+        let errorStyle = Style [ Color "crimson"; FontSize 12 ]
+
+        ul [] [
+            for error in errors -> li [ errorStyle ] [ str error ]
+        ]
 
 let appIcon =
     img [ Src "/img/favicon-book.png"
-          Style [ Height 60
-                  Width 80 ] ]
+          Style [ Height 60; Width 80 ] ]
 
-let render (state : State) dispatch =
+let render (state: State) dispatch =
     let loginBtnContent =
-        if state.LoggingIn then i [ ClassName "fa fa-circle-o-notch fa-spin" ] []
-        else str "Login"
-    
+        if state.LoggingIn then
+            i [ ClassName "fa fa-circle-o-notch fa-spin" ] []
+        else
+            str "Login"
+
     let btnClass =
-        if state.CanLogin then "btn btn-success btn-lg"
-        else "btn btn-info btn-lg"
-    
+        if state.CanLogin then
+            "btn btn-success btn-lg"
+        else
+            "btn btn-info btn-lg"
+
     div [ ClassName "container"
-          loginFormStyle ] 
-        [ div [ ClassName "card" ] 
-              [ div [ ClassName "card-block"
-                      cardBlockStyle ] 
-                    [ h1 [ Style [ TextAlign "center" ] ] [ str "Tabula Rasa" ]
-                      div [ Style [ TextAlign "center" ] ] [ appIcon ]
-                      br []
-                      textInput "Username" state.InputUsername Text (ChangeUsername >> dispatch)
-                      errorMessagesIfAny state.HasTriedToLogin state.UsernameValidationErrors
-                      textInput "Password" state.InputPassword Password (ChangePassword >> dispatch)
-                      errorMessagesIfAny state.HasTriedToLogin state.PasswordValidationErrors
-                      
-                      div [ Style [ TextAlign "center" ] ] 
-                          [ button [ ClassName btnClass
-                                     OnClick(fun _ -> dispatch Login) ] [ loginBtnContent ] ] ] ] ]
+          loginFormStyle ] [
+        div [ ClassName "card" ] [
+            form [ ClassName "card-block"
+                   cardBlockStyle ] [
+                h1 [ Style [ TextAlign "center" ] ] [
+                    str "Tabula Rasa"
+                ]
+                div [ Style [ TextAlign "center" ] ] [
+                    appIcon
+                ]
+                br []
+                textInput "Username" state.InputUsername Text (ChangeUsername >> dispatch)
+                errorMessagesIfAny state.HasTriedToLogin state.UsernameValidationErrors
+                textInput "Password" state.InputPassword Password (ChangePassword >> dispatch)
+                errorMessagesIfAny state.HasTriedToLogin state.PasswordValidationErrors
+
+                div [ Style [ TextAlign "center" ] ] [
+                    button [ ClassName btnClass
+                             OnClick(fun ev -> dispatch Login) ] [
+                        loginBtnContent
+                    ]
+                ]
+            ]
+        ]
+    ]
